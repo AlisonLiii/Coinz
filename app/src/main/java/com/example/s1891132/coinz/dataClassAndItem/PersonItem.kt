f8f5c5689adf260285f2de94efc6ea4a827dff6a
s1891132 <s1891132@ed.ac.uk>
2018-12-05 10:44:01 +0000

    Fix all the warnings in class PersonItem

--- app/src/main/java/com/example/s1891132/coinz/dataClassAndItem/PersonItem.kt
+++ app/src/main/java/com/example/s1891132/coinz/dataClassAndItem/PersonItem.kt
@@ -6,6 +6,9 @@ import android.content.Context
 import com.example.s1891132.coinz.R
 import kotlinx.android.synthetic.main.item_person.*
 
+//this class follows the tutorial below
+//https://www.youtube.com/watch?v=a9I7Ppzh1_Y
+
 class PersonItem(val person: CoinzUser,
                  private val context: Context)
     : Item() {
@@ -13,10 +16,10 @@ class PersonItem(val person: CoinzUser,
     override fun bind(viewHolder: ViewHolder, position: Int) {
         viewHolder.person_name_item.text = person.name
         viewHolder.person_bio_item.text = person.bio
-        if(person.camp==0.0)
-            viewHolder.person_camp_item.text="camp:\n"+"AI"
+        if(person.camp==0.0)//camp=0.0->the user belongs to AI, camp=1.0->the user belongs to human
+            viewHolder.person_camp_item.text=context.getString(R.string.camp_is_AI)
         else
-            viewHolder.person_camp_item.text="camp:\n"+"Human"
+            viewHolder.person_camp_item.text=context.getString(R.string.camp_is_human)
     }
 
     override fun getLayout() = R.layout.item_person
